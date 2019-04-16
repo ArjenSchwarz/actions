a998cf9debb30ce522268eeba6b1af4fdaa6ba9a
Arjen Schwarz <arjen.schwarz@digio.com.au>
2019-04-16 10:11:41 +1000

    Disable ghr version output and add more debugging

--- github/release/entrypoint.sh
+++ github/release/entrypoint.sh
@@ -14,20 +14,25 @@ fi
 [ -n "$SOURCE_PATH" ] || export SOURCE_PATH="output"
 [ -n "$VERSION" ] || export VERSION="pre-release"
 
+echo $SOURCE_PATH
+echo $VERSION
+
 LATEST_GHR=$(curl -H "Authorization: token ${GITHUB_TOKEN}" -s https://api.github.com/repos/tcnksm/ghr/releases/latest | jq -r '.assets | map(select(.browser_download_url | contains ("linux_amd64")))[0].browser_download_url')
 output="Downloading latest version of ghr: ${LATEST_GHR}"
 curl -Ls "$LATEST_GHR" -o /tmp/ghr_latest.tar.gz
 mkdir -p /tmp/latest && tar xzf /tmp/ghr_latest.tar.gz --directory /tmp/latest
 mv /tmp/latest/*/ghr /bin/ghr
 
-output="$output\nghr version output: $(ghr version)"
+ls -lh /bin/ghr
+
+# output="$output\nghr version output: $(ghr version)"
 
 # Split GITHUB_REPOSITORY into user and repo
 IFS='/' read -r -a REPO_ARRAY <<< "${GITHUB_REPOSITORY}"
 
 output="$output\nBuilding with command: ghr -t ${GITHUB_TOKEN} -u ${REPO_ARRAY[0]} -r ${REPO_ARRAY[1]} --debug $* ${VERSION} ${SOURCE_PATH}"
 echo -e "$output"
-# output="$output\n$(ghr -t "${GITHUB_TOKEN}" -u "${REPO_ARRAY[0]}" -r "${REPO_ARRAY[1]}" --debug "$*" "${VERSION}" "${SOURCE_PATH}")"
+output="$output\n$(ghr -t "${GITHUB_TOKEN}" -u "${REPO_ARRAY[0]}" -r "${REPO_ARRAY[1]}" --debug "$*" "${VERSION}" "${SOURCE_PATH}")"
 # Capture output
 
 # Write output to STDOUT
